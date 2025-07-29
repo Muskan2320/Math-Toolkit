0dd5cfa7292ffb62514fe17db609ae2e1086a93d
muskan2320 <muskan1802.m@gmail.com>
2025-07-29 17:50:12 +0530

    updated server for rendering interface

--- Web-Math-Toolkit/server.mjs
+++ Web-Math-Toolkit/server.mjs
@@ -9,7 +9,7 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(express.json());
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.post('/calculate', (req, res) => {
     const { operation, num1, num2 } = req.body;
