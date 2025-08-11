378dff20ece2381c79b10c4e09be23f5cdf1a2ea
muskan2320 <muskan1802.m@gmail.com>
2025-08-11 23:04:14 +0530

    updated division logic - rounding number to 2 decimal place

--- Web-Math-Toolkit/math.mjs
+++ Web-Math-Toolkit/math.mjs
@@ -1,5 +1,5 @@
 export function add(a, b) { return a + b; }
 export function subtract(a, b) { return a - b; }
 export function multiply(a, b) { return a * b; }
-export function divide(a, b) { return b === 0 ? "Cannot divide by zero" : a / b; }
+export function divide(a, b) { return b === 0 ? "Cannot divide by zero" : Number((a / b).toFixed(2)); }
 export function square(a) { return a * a; }
